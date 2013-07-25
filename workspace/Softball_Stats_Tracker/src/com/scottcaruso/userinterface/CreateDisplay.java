667eabaf50fbee788da285712e4c8e630a6faf48
Scott Caruso <scottcaruso@fullsail.edu>
2013-07-25 03:39:32 -0400

    Interface manipulation nearly complete. Grid layout done.

--- workspace/Softball_Stats_Tracker/src/com/scottcaruso/userinterface/CreateDisplay.java
+++ workspace/Softball_Stats_Tracker/src/com/scottcaruso/userinterface/CreateDisplay.java
@@ -12,76 +12,75 @@ import com.scottcaruso.statcalculation.Averages;
 import android.content.Context;
 import android.util.Log;
 import android.view.Gravity;
+import android.widget.GridLayout;
 import android.widget.TextView;
 
 
 /*This class is exclusively for creating the display that shows the statistics at the bottom of the screen when the user clicks the
  *appropriate button.
  */
-public class CreateDisplay {
+public class CreateDisplay extends GridLayout {
 	
-	public static TextView createStatDisplay(JSONObject object, Context context)
+	TextView date;
+	TextView opponent;
+	TextView atbats;
+	TextView hits;
+	TextView doubles;
+	TextView triples;
+	TextView homeruns;
+	TextView rbi;
+	Context context;
+	
+	
+	public CreateDisplay(Context thisContext, int numberOfGames)
 	{
-		JSONObject firstObject = null;
-		JSONObject secondObject = null;
-		JSONObject thirdObject = null;
-		try {
-			firstObject = object.getJSONObject("Game 1");
-			secondObject = object.getJSONObject("Game 2");
-			thirdObject = object.getJSONObject("Game 3");
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		JSONObject [] arrayOfObjects = {firstObject,secondObject,thirdObject};
+		super(thisContext);
 		
-		TextView thisTextView = new TextView (context);
-		thisTextView.setGravity(Gravity.CENTER);
-		for (int x = 1; x <= arrayOfObjects.length; x++)
-		{
-			JSONObject currentObject = arrayOfObjects[x-1];
-			String gameNumber = String.valueOf(x);
-			String opponent = null;
-			String date = null;
-			String atBats = null;
-			String hits = null;
-			String doubles = null;
-			String triples = null;
-			String homeruns = null;
-			String rbis = null;
-			//The floats need to be configured to use the appropriate number of characters still.
-			float average = 0.0f;
-			float slugging = 0.0f;
-			try {
-				opponent = currentObject.getString("Opponent");
-				date = currentObject.getString("Date");
-				int intAtBats = currentObject.getInt("At Bats");
-				atBats = String.valueOf(intAtBats);
-				int intHits = currentObject.getInt("Hits");
-				hits = String.valueOf(intHits);
-				int intDoubles = currentObject.getInt("Doubles");
-				doubles = String.valueOf(intDoubles);
-				int intTriples = currentObject.getInt("Triples");
-				triples = String.valueOf(intTriples);
-				int intHomers = currentObject.getInt("Home Runs");
-				homeruns = String.valueOf(intHomers);
-				int intRBI = currentObject.getInt("RBIs");
-				rbis = String.valueOf(intRBI);
-				//Use Averages class to get BA and slugging percentages.
-				average = Averages.battingAverage(intAtBats, intHits);
-				slugging = Averages.sluggingPercentage(intAtBats, intHits, intDoubles, intTriples, intHomers);
-			} catch (JSONException e) {
-				String debugString = (opponent + " " + date + " " + atBats + " " + hits + " " + doubles + " " + triples + " " + homeruns + " " + rbis);
-						Log.i("Strings: ",debugString);
-			}
-			thisTextView.append("Game #" + gameNumber + "\r\n"
-								+ "Opponent: " + opponent + "\r\n"
-								+ "Date: " + date + "\r\n"
-								+ "At Bats: " + atBats + "  Hits: " + hits + "  Doubles: " + doubles + "  Triples: " + triples +
-								"  Home Runs: " + homeruns + "  RBIs: " + rbis + 
-								" Batting Average: " + average + "  Slugging: " + slugging + "\r\n");
-		}
+		context = thisContext;
+		this.setColumnCount(8);
+		this.setRowCount(numberOfGames);
 		
-		return thisTextView;
-	}
+		TextView dateLabel = new TextView(context);
+		dateLabel.setText("Date");
+		date = new TextView(context);
+		TextView opponentLabel = new TextView(context);
+		opponentLabel.setText("Opponent");
+		opponent = new TextView(context);
+		TextView atbatLabel = new TextView(context);
+		atbatLabel.setText("AB");
+		atbats = new TextView(context);
+		TextView hitsLabel = new TextView(context);
+		hitsLabel.setText("H");
+		hits = new TextView(context);
+		TextView doublesLabel = new TextView(context);
+		doublesLabel.setText("2B");
+		doubles = new TextView(context);
+		TextView triplesLabel = new TextView(context);
+		triplesLabel.setText("3B");
+		triples = new TextView(context);
+		TextView homerunsLabel = new TextView(context);
+		homerunsLabel.setText("HR");
+		homeruns = new TextView(context);
+		TextView rbiLabel = new TextView(context);
+		rbiLabel.setText("RBI");
+		rbi = new TextView(context);
 	
+		this.addView(dateLabel);
+		this.addView(date);
+		this.addView(opponentLabel);
+		this.addView(opponent);
+		this.addView(atbatLabel);
+		this.addView(atbats);
+		this.addView(hitsLabel);
+		this.addView(hits);
+		this.addView(doublesLabel);
+		this.addView(doubles);
+		this.addView(triplesLabel);
+		this.addView(triples);
+		this.addView(homerunsLabel);
+		this.addView(homeruns);
+		this.addView(rbiLabel);
+		this.addView(rbi);
+	}
+		
 }
